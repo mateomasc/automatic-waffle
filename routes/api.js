89bebe5df8a85237f84244f4d96e4d10c6497e7e
mateomasc <matiusmascieri7@gmail.com>
2025-11-09 21:50:39 -0300

    Cambiar res.json a res.send para mensajes de error

--- routes/api.js
+++ routes/api.js
@@ -15,15 +15,15 @@ module.exports = function (app) {
       
       // Validar número y unidad
       if (initNum === 'invalid number' && initUnit === 'invalid unit') {
-        return res.json('invalid number and unit');
+        return res.send('invalid number and unit');
       }
       
       if (initNum === 'invalid number') {
-        return res.json('invalid number');
+        return res.send('invalid number');
       }
       
       if (initUnit === 'invalid unit') {
-        return res.json('invalid unit');
+        return res.send('invalid unit');
       }
       
       // Realizar conversión
